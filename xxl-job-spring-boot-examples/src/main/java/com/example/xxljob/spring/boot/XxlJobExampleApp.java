8dec31a48e502f455143841f841ab08d3ea270a2
ytc <6517390+one_eye_years@user.noreply.gitee.com>
2022-04-22 17:50:40 +0800

    init xxljob example

---
+++ xxl-job-spring-boot-examples/src/main/java/com/example/xxljob/spring/boot/XxlJobExampleApp.java
@@ -0,0 +1,15 @@
+package com.example.xxljob.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExampleApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExampleApp.class, args);
+    }
+
+
+}
---
+++ xxl-job-spring-boot-examples/src/main/java/com/example/xxljob/spring/boot/controller/XxlJobExampleController.java
@@ -0,0 +1,12 @@
+package com.example.xxljob.spring.boot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class XxlJobExampleController {
+
+
+
+
+
+}
