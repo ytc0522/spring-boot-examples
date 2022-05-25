4f33007abe436c3c7b648811e7ab31431ea0d9e1
ytc0522 <6517390+one_eye_years@user.noreply.gitee.com>
2022-05-25 13:59:38 +0800

    seata 修改 datasource 配置

--- dubbo-springboot-examples/service-provider/src/main/java/com/example/dubbo/provider/config/SeataConfig.java
+++ dubbo-springboot-examples/service-provider/src/main/java/com/example/dubbo/provider/config/SeataConfig.java
@@ -1,20 +1,26 @@
 package com.example.dubbo.provider.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import io.seata.rm.datasource.DataSourceProxy;
 import io.seata.spring.annotation.GlobalTransactionScanner;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class SeataConfig {
 
-    //@Bean
-    public DataSourceProxy dataSourceProxy(DataSource dataSource) {
+    @Bean
+    public DataSourceProxy dataSourceProxy(DataSourceProperties dataSourceProperties) {
+        HikariDataSource dataSource = dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+        if (StringUtils.hasText(dataSourceProperties.getName())) {
+            dataSource.setPoolName(dataSourceProperties.getName());
+        }
         return new DataSourceProxy(dataSource);
     }
 
-    //@Bean
+    @Bean
     public GlobalTransactionScanner globalTransactionScanner() {
         return new GlobalTransactionScanner("stock-seata-example", "my_test_tx_group");
     }
