3fdeb4edc506bb3ca2a21fb9fb9e3352b3f31326
ytc0522 <6517390+one_eye_years@user.noreply.gitee.com>
2022-05-30 16:57:01 +0800

    Rocketmq 消费模式

--- rocketmq-example/src/main/java/org/example/rocketmq/consumer/Consumer.java
+++ rocketmq-example/src/main/java/org/example/rocketmq/consumer/Consumer.java
@@ -6,6 +6,7 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
 import java.util.List;
 
@@ -22,17 +23,21 @@ public class Consumer {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("group1");
         // 2 指定namserver 地址
         consumer.setNamesrvAddr("127.0.0.1:9876");
+        // 设置消费模式 默认是负载均衡模式，可指定为广播模式
+        consumer.setMessageModel(MessageModel.BROADCASTING);
+
         //3 订阅topic和tag
         String topic = "base";
-        String tag = "tag1";
-        consumer.subscribe(topic, tag);
+        // tag 表达式
+        String tagExpress = "tag1";
+        consumer.subscribe(topic, tagExpress);
         // 4 设置回调函数
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgList, ConsumeConcurrentlyContext context) {
                 // 接受消息
                 for (MessageExt messageExt : msgList) {
-                    log.info("接收到消息：{}", messageExt);
+                    //log.info("接收到消息：{}", messageExt);
                     log.info("消息Body：{}", new String(messageExt.getBody()));
                 }
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
